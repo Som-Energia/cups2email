c5a9e600b2c4dcf828111b78e911c10a12b022c9
cesarlr <cesar.lopez@somenergia.coop>
2016-06-15 12:54:16 +0200

    Add continue to except block to skip bad cases

--- cups2email_lang.py
+++ cups2email_lang.py
@@ -27,10 +27,12 @@ def cups2email(template=None,filename=None):
             cups_id = cups_obj.search([('name','=',cups)])[0]
         except:
             print "El CUPS no existe"
+            continue
         try:
             pol_id = pol_obj.search([('cups','=',cups_id)])[0]
         except:
             print "No hay pólizas activas asociadas al CUPS"
+            continue
         pol=pol_obj.get(pol_id)
         print pol.pagador.lang
         print pol.pagador.www_email
