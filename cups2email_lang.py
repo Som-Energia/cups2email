6380ad5ab21925756f32a0aab83f2cd6e97c2175
Juan Pedro Sánchez <juanpe.sn@gmail.com>
2018-11-12 18:00:10 +0100

    cups2email_lang: "from" is now taken from the template. cups2email_lang_c: script to send second notice for the electric installation certificate added .gitignore

--- cups2email_lang.py
+++ cups2email_lang.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from ooop import OOOP
 import sys
+
 import configdb
+from ooop import OOOP
 from openpyxl import load_workbook
 
-def cups2email(template=None,filename=None):
+
+def cups2email(template=None, filename=None):
     if not filename:
         sys.exit("No filename defined")
     if not template:
@@ -16,41 +18,44 @@ def cups2email(template=None,filename=None):
     O = OOOP(**configdb.ooop)
     pol_obj = O.GiscedataPolissa
     cups_obj = O.GiscedataCupsPs
-    templ_obj=O.PoweremailTemplates
-    templ_id=templ_obj.search([('name','=',template)])
-#Skip header row
-    row=2
-    while ws.cell(row=row,column=1).value:
-        cups=ws.cell(row=row,column=1).value
+    templ_obj = O.PoweremailTemplates
+    templ_id = templ_obj.search([('name', '=', template)])
+    templ_info = templ_obj.read(templ_id)
+
+    # Skip header row
+    row = 2
+    while ws.cell(row=row, column=1).value:
+        cups = ws.cell(row=row, column=1).value
         print cups
         try:
-            cups_id = cups_obj.search([('name','=',cups)])[0]
+            cups_id = cups_obj.search([('name', '=', cups)])[0]
         except:
             print "El CUPS no existe"
-            row+=1
+            row += 1
             continue
         try:
-            pol_id = pol_obj.search([('cups','=',cups_id)])[0]
+            pol_id = pol_obj.search([('cups', '=', cups_id)])[0]
             print pol_id
         except:
             print "No hay pólizas activas asociadas al CUPS"
-            row+=1
+            row += 1
             continue
-        #pol=pol_obj.get(pol_id)
-        #print pol.pagador.lang
-        #print pol.pagador.www_email
-        ctx = {'active_ids': [pol_id],
-              'active_id': pol_id,
-              'template_id': templ_id,
-              'src_model': 'giscedata.polissa',
-              'src_rec_ids': [pol_id],
-              'from': 1}
-        params = {'state': 'single',
-                 'priority':0,
-                 'from': ctx['from']}
+        ctx = {
+            'active_ids': [pol_id],
+            'active_id': pol_id,
+            'template_id': templ_id,
+            'src_model': 'giscedata.polissa',
+            'src_rec_ids': [pol_id],
+            'from': templ_info[0]['enforce_from_account'][0]
+        }
+        params = {
+            'state': 'single',
+            'priority': 0,
+            'from': ctx['from']
+        }
         try:
-           wz_id = O.PoweremailSendWizard.create(params, ctx)
-           O.PoweremailSendWizard.send_mail([wz_id], ctx)
+            wz_id = O.PoweremailSendWizard.create(params, ctx)
+            O.PoweremailSendWizard.send_mail([wz_id], ctx)
         except Exception:
-           raise
-        row+=1
+            raise
+        row += 1
